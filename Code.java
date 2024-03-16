cadcc47cc948d8e48ff29c119807f18e1703bbcb
JDX02 <j.dixon12@lancaster.ac.uk>
2024-03-16 21:00:44 +0000

    Polished Java - template to expand

--- Code.java
+++ Code.java
@@ -1,6 +1,4 @@
-// This is our shared main file
-
-import java.sql.*; 
+import java.sql.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,90 +6,59 @@ import java.net.ConnectException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CharliesReader{
+public class Code {
+    private static class CharlieReader {
+        public CharlieReader() {
+            String databaseName = "CharlieDB";
+            String username = "Charlie";
+            String password = "38700514";
 
-    public CharliesReader(){
-        Connection connection = CreateConnection();
-        CreateDatabase(connection);
-        CreateTables(connection);
-        ReadCSV();
-        PopulateTables();
-        RunQueries();
+            createDatabase(databaseName, username, password);
+        }
     }
 
-	public String databaseName = "CharliesDB";
+    private static class JoeReader {
+        public JoeReader() {
+            String databaseName = "JoeDB";
+            String username = "Joe";
+            String password = "38771225";
 
-    public Connection CreateConnection(){
+            createDatabase(databaseName, username, password);
+        }
+    }
+
+    private static class KaweeshaReader {
+        public KaweeshaReader() {
+            String databaseName = "KaweeshaDB";
+            String username = "Kaweesha";
+            String password = "39166236";
+
+            createDatabase(databaseName, username, password);
+        }
+    }
+
+    public static void createDatabase(String name, String username, String password) {
         String jdbcUrl = "jdbc:mysql://localhost:3306/";
-        Connection connection = null;
-		try
-		{ 
-			connection = DriverManager.getConnection(jdbcUrl);
-		}
-		catch (SQLException e) 
-		{
-		    e.printStackTrace();
-		}		
-		return connection;
-    }
 
-    public void CreateDatabase(Connection connection){
-        try{
-		    Statement statement = connection.createStatement();
-		    String createDatabase = "CREATE DATABASE IF NOT EXISTS " + databaseName;
-		    statement.executeUpdate(createDatabase);
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-
-    public void CreateTables(Connection connection){
-        try {
+        try (
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
             Statement statement = connection.createStatement();
+        ) {
+            String createDatabaseQuery = "CREATE DATABASE " + name;
+            statement.executeUpdate(createDatabaseQuery);
+            System.out.println("Database '" + name + "' created successfully.");
 
-            // Use the database:
-            String UseDatabase = "use " + databaseName;
-            statement.executeUpdate(UseDatabase);
-
-            // Now create tables:
-            String createManagerTable = "CREATE TABLE IF NOT EXISTS Manager(Manager_ID INTEGER, First_Name VARCHAR(50), Last_Name VARCHAR(50), Age INTEGER, PRIMARY KEY (Manager_ID))";
-            statement.executeUpdate(createManagerTable);
-            String createTeamTable = "CREATE TABLE IF NOT EXISTS Team(Team_ID INTEGER, Team_Name VARCHAR(100), Team_Abbreviation VARCHAR(5), Year_Founded INTEGER, Manager_ID INTEGER NOT NULL, PRIMARY KEY(Team_ID)), FOREIGN KEY(Manager_ID) REFERENCES Manager(Manager_ID))";
-		    statement.executeUpdate(createTeamTable);
-            String createPlayerTable = "CREATE TABLE IF NOT EXISTS Player(Player_ID INTEGER, First_Name VARCHAR(50), Last_Name VARCHAR(50), Shirt_Number INTEGER, Age INTEGER, Team_ID INTEGER, PRIMARY KEY(Player_ID), FOREIGN KEY(Team_ID) REFERENCES Team(Team_ID))";
-            statement.executeUpdate(createPlayerTable);
-        }
-        catch (SQLException e){
+            String useDatabaseQuery = "USE " + name;
+            statement.executeUpdate(useDatabaseQuery);
+            System.out.println("Database '" + name + "' is now in use.");
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void ReadCSV(){
-
-    }
-
-    public void PopulateTables(){
-
-    }
-
-    public void RunQueries(){
-
-    }
-}
-
-public class JoesReader{
-
-}
-
-public class KaweeshasReader{
-
-}
-
-public class Main{
-    public static void main(String[] args){
-        CharliesReader charlie = new CharliesReader();
-        JoesReader joe;
-        KaweeshasReader kaweesha;
+    public static void main(String[] args) {
+        CharlieReader charlie = new CharlieReader();
+        JoeReader joe = new JoeReader();
+        KaweeshaReader kaweesha = new KaweeshaReader();
     }
 }
